33ddfeac41631f7be15bd496e553acf7f12620fd
jhkroger <jhkroger2@gmail.com>
2019-12-14 18:12:25 +0200

    Type agnostic add

--- vec2.go
+++ vec2.go
@@ -10,24 +10,37 @@ type Vec2 struct {
 }
 
 
-func (v *Vec2) Add(o Vec2) {
+func (v *Vec2) Add(i interface{}) {
+	switch o := i.(type) {
+	case Vec2:
+		v.AddV(o)
+	case int:
+		v.AddS(float64(o))
+	case float32:
+		v.AddS(float64(o))
+	default:
+		panic("Incompatible type!")
+	}
+}
+
+func (v *Vec2) AddV(o Vec2) {
 	v.X += o.X
 	v.Y += o.Y
 }
 
-func (v *Vec2) Sub(o Vec2) {
-	v.X -= o.X
-	v.Y -= o.Y
+func (v *Vec2) AddS(o float64) {
+	v.X += o
+	v.Y += o
 }
 
 func (v *Vec2) Mul(i interface{}) {
 	switch o := i.(type) {
+	case Vec2:
+		v.MulV(o)
 	case int:
 		v.MulS(float64(o))
 	case float32:
 		v.MulS(float64(o))
-	case Vec2:
-		v.MulV(o)
 	default:
 		panic("Incompatible type!")
 	}
@@ -43,6 +56,12 @@ func (v *Vec2) MulS(o float64) {
 	v.X *= o
 	v.Y *= o
 }
+
+func (v *Vec2) Sub(o Vec2) {
+	v.X -= o.X
+	v.Y -= o.Y
+}
+
 
 func (v *Vec2) Project(o Vec2) {
 	dot := v.Dot(o)
